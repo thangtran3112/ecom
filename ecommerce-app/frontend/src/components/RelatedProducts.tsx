cdf182c7f5902c098275b2169bd3915a893f786b
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-09-14 14:48:48 -0700

    Add related Products

---
+++ ecommerce-app/frontend/src/components/RelatedProducts.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useContext, useEffect, useState } from "react";
+import Title from "./Title";
+import { ShopContext } from "../context/ShopContext";
+import ProductItem from "./ProductItem";
+import { Product } from "../interfaces/Product";
+
+interface RelatedProductsProps {
+  category: string;
+  subCategory: string;
+}
+
+const RelatedProducts = ({ category, subCategory }: RelatedProductsProps) => {
+  const [related, setRelated] = useState<Product[]>([]);
+
+  const { products } = useContext(ShopContext);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      const productsCopy = products
+        .slice()
+        .filter((item) => category === item.category)
+        .filter((item) => subCategory === item.subCategory);
+
+      // Only display 5 related products
+      setRelated(productsCopy.slice(0, 5));
+    }
+  }, [products]);
+
+  return (
+    <div className="my-24">
+      <div className="text-center text-3xl py-2">
+        <Title text1="RELATED" text2="PRODUCTS" />
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {related.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RelatedProducts;
