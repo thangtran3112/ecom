48b51dcee4dcd5300c8121943e5bcdd8d625f82b
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-09-20 21:26:24 -0700

    added Add Product Form

--- fashionify/fullstack/admin/src/common/constants.ts
+++ fashionify/fullstack/admin/src/common/constants.ts
@@ -1,2 +1,21 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const TOKEN = "token";
+export enum Sizes {
+  S = "S",
+  M = "M",
+  L = "L",
+  XL = "XL",
+  XXL = "XXL",
+}
+
+export enum Categories {
+  Men = "Men",
+  Women = "Women",
+  Kids = "Kids",
+}
+
+export enum SubCategories {
+  Topwear = "Topwear",
+  Bottomwear = "Bottomwear",
+  Winterwear = "Winterwear",
+}
