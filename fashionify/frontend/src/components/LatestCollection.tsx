badda9b06146116de8fa8bfe9f1706c5c0118088
Thang Tran <thangtran3112@gmail.com>
2024-12-14 14:49:04 -0800

    adjusted title

--- fashionify/frontend/src/components/LatestCollection.tsx
+++ fashionify/frontend/src/components/LatestCollection.tsx
@@ -16,7 +16,7 @@ const LatestCollection = () => {
   return (
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
-        <Title text1="LATEST" text2="COLLECTIONS" />
+        <Title text1="LATEST" text2="ARRIVALS" />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the.
