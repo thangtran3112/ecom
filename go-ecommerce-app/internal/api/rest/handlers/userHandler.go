82cd1944cd6b1b8a0569233de79433276909fc67
Toby Tran <ttran@stackline.com>
2025-06-04 21:34:32 -0700

    basic handlers in fiber app

---
+++ go-ecommerce-app/internal/api/rest/handlers/userHandler.go
@@ -0,0 +1,105 @@
+package handlers
+
+import (
+	"go-ecommerce-app/internal/api/rest"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type UserHandler struct {
+	// svc UserService
+}
+
+func SetupUserRoutes(rh *rest.RestHandler) {
+	app := rh.App
+
+	// create an instance of user service & inject to handler
+	handler := UserHandler{}
+
+	// Public endpoints
+	app.Post("/register", handler.Register)
+	app.Post("/login", handler.Login)
+
+	// Protected endpoints
+	app.Get("/verify", handler.GetVerificationCode)
+	app.Post("/verify", handler.Verify)
+
+	app.Post("/profile", handler.CreateProfile)
+	app.Get("/profile", handler.GetProfile)
+	app.Patch("/profile", handler.UpdateProfile)
+
+	app.Post("/cart", handler.AddToCart)
+	app.Get("/cart", handler.GetCart)
+
+	app.Get("/order", handler.GetOrders)
+	app.Get("/order/:id", handler.GetOrder)
+
+	app.Post("/become-seller", handler.BecomeSeller)
+
+}
+
+func (h *UserHandler) Register(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "registration successful",
+	})
+}
+
+func (h *UserHandler) Login(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "login successful",
+	})
+}
+
+func (h *UserHandler) GetVerificationCode(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "verify successful",
+	})
+}
+
+func (h *UserHandler) Verify(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "verify successful",
+	})
+}
+
+func (h *UserHandler) CreateProfile(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "profile created successfully",
+	})
+}
+func (h *UserHandler) GetProfile(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "profile retrieved successfully",
+	})
+}
+func (h *UserHandler) UpdateProfile(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "profile updated successfully",
+	})
+}
+func (h *UserHandler) AddToCart(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "item added to cart successfully",
+	})
+}
+func (h *UserHandler) GetCart(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "cart retrieved successfully",
+	})
+}
+func (h *UserHandler) GetOrders(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "orders retrieved successfully",
+	})
+}
+func (h *UserHandler) GetOrder(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "order retrieved successfully",
+	})
+}
+func (h *UserHandler) BecomeSeller(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "seller registration successful",
+	})
+}
--- go-ecommerce-app/internal/api/server.go
+++ go-ecommerce-app/internal/api/server.go
@@ -2,18 +2,32 @@ package api
 
 import (
 	"go-ecommerce-app/config"
+	"go-ecommerce-app/internal/api/rest"
 	"net/http"
+	"go-ecommerce-app/internal/api/rest/handlers"
 	"github.com/gofiber/fiber/v2"
 )
 
 func StartServer(config config.AppConfig) {
 	// Initialize the Fiber app
 	app := fiber.New()
-	app.Get("/health", HealthCheck)
+	// modify the Fiber app (like adding routes) and ensures working with 
+	// the same app instance when using &
+	restHandler := &rest.RestHandler{
+		App: app,
+	}
+	setupRoutes(restHandler)
 	app.Listen(config.ServerPort)
 }
 func HealthCheck(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
 		"message" : "I am breathing!",
 	})
+}
+
+func setupRoutes(rh *rest.RestHandler) {
+	// user handler
+	handlers.SetupUserRoutes(rh)
+	// transactions
+	// catalog
 }
